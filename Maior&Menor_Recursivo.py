62a89fa4d85b7a8741542f1b2e50855836fea465
5nyght <47366440+wesleysousaa@users.noreply.github.com>
2022-03-24 10:30:57 -0300

    update

--- Maior&Menor_Recursivo.py
+++ Maior&Menor_Recursivo.py
@@ -2,27 +2,25 @@ def numInt(lista, op):
     if (op == 1):
         if(len(lista) == 1):
             return lista[0]
-        elif (lista[0] > numInt(lista[1:], op)):
-            maior = lista[0]
         else:
-            return numInt(lista[1:], op)
-        return maior
+          if(lista[0] > numInt(lista[1:], op)):
+            return lista[0]
+          return numInt(lista[1:], op)
     elif (op == 2):
         if (len(lista) == 1):
             return lista[0]
-        elif (lista[0] < numInt(lista[1:], op)):
-            menor = lista[0]
         else:
-            return numInt(lista[1:], op)
-        return menor
-lista = [10, -1, 9, 2, 20, 12]
+          if (lista[0] < numInt(lista[1:], op)):
+            return lista[0]
+          return numInt(lista[1:], op)
+lista = [10, -1, 9, 2, 20, 120]
 
 op = 3
 msg = ""
 
 while (op != 0):
     print(f"Lista : {lista}")
-    op = int(input("1 - Ver maior número da lista\n2 - Ver menor nomuero da lista\n0 - Sair\n"))
+    op = int(input("1 - Ver maior número da lista\n2 - Ver menor nomero da lista\n0 - Sair\n"))
     if (op == 1):
         msg = "O maior numero é "
     elif (op == 2):
